6aad33d8e14382fbd76a7687e78b9e647d3fef84
rosso101 <victorprivv101@gmail.com>
2023-11-30 11:04:45 +0300

    completed dog assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+##This ensures that when you create an instance of the Dog class, you can pass a name and optionally a breed,
+# with the breed defaulting to "Mutt" if not provided.
+   def __init__(self, name, breed="Mutt"):
+      self.name = name
+      self.breed = breed
+    
+    fido = Dog("Fido")
+    print(Fido.name)
+    print(fido.breed)
+   
+   snoopy = Dog("Snoopy", "Beagle")
+    print(snoopy.name)
+    print(snoopy.breed) 
